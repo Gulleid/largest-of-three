062741ea5f174194a6992fdf49405e32b0b1c816
Gulleid <gulleidmohamed93@gmail.com>
2023-05-25 16:12:42 -0800

    compare three Numbers

--- challenge.js
+++ challenge.js
@@ -1,2 +1,10 @@
 // Good Luck! You got this 💪🏾
 // Write your code here.
+const x =20
+const y =45
+const z =55
+
+
+if(x>y && x>z) console.log("X = "+x+" the greastest number out of three is ");
+    else if(z>x && z>y) console.log("Z = "+z+" the greastest number out of three is");
+    else console.log("Y = "+y+" the greastest number out of three is");
